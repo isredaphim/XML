8bef79e4ddec99a762a087c292002a77615f7a33
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-02-12 14:41:30 +0800

    Add files via upload

---
+++ rss.php
@@ -0,0 +1,16 @@
+<?php
+	
+	$object = new DOMDocument();
+	$object->load("https://exerxml.herokuapp.com/");
+	
+	
+	$content = $object->getElementsByTagName("item");
+	
+	foreach($content as $data){
+			$name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+			$age = $data->getElementsByTagName("age")->item(0)->nodeValue;
+			$gender = $data->getElementsByTagName("gender")->item(0)->nodeValue;
+			
+	
+		echo "$name - $age - $gender <br>";
+	}
