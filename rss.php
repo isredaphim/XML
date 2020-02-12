46486076c01a34c0cb744703711c4962acbe001f
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-02-12 14:49:17 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -7,11 +7,11 @@
 	$content = $object->getElementsByTagName("item");
 	
 	foreach($content as $data){
-			$ID = $data->getElementsByTagName("id")->item(0)->nodeValue;
+			$id = $data->getElementsByTagName("id")->item(0)->nodeValue;
 			$firstname = $data->getElementsByTagName("firstname")->item(0)->nodeValue;
 			$age = $data->getElementsByTagName("age")->item(0)->nodeValue;
 			$gender = $data->getElementsByTagName("gender")->item(0)->nodeValue;
 			
 	
-		echo "$ID - $firstname - $age - $gender <br>";
+		echo "$id - $firstname - $age - $gender <br>";
 	}
